60b2347df7c6b805612df23cd00122004cad085e
randolphcyg <cyg0054@gmail.com>
2023-03-07 22:10:35 +0800

    [ADD]add gitignore and test case

--- parser_test.go
+++ parser_test.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,4 +26,39 @@ func TestParseNmap(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println(string(probesJSON))
+}
+
+func TestParseNmapAndToJson(t *testing.T) {
+	readable := true
+	srcFilePath := "nmap-service-probes"
+	probes, err := ParseNmap(srcFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Convert the probes slice to JSON
+	probesJSON, err := json.Marshal(probes)
+	if err != nil {
+		panic(err)
+	}
+
+	// Save the probes JSON to a file
+	err = os.WriteFile(srcFilePath+".json", probesJSON, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	if readable {
+		// Convert the probes slice to JSON
+		probesJSON, err := json.MarshalIndent(probes, "", "    ")
+		if err != nil {
+			panic(err)
+		}
+
+		// Save the probes JSON to a file
+		err = os.WriteFile(srcFilePath+"_Readable.json", probesJSON, 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
